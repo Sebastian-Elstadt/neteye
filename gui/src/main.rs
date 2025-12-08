7cde9da17c66afcf222567261c938e17ac282d10
Sebastian Elstadt <sebastian@elstadt.com>
2025-12-08 20:06:24 +0200

    simple gui

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,11 +1,32 @@
+// use std::io::{self, Write};
+use std::process::{Command, Stdio};
+
+use slint::ToSharedString;
+
 slint::include_modules!();
 
 fn main() {
-    let ui = AppWindow::new().unwrap();
+    let app = AppWindow::new().unwrap();
+    let app_ref = app.as_weak();
 
-    ui.on_scan_ports(move |net_addr: slint::SharedString| {
-        // run_network_scan(&net_access, &net_addr);
+    app.on_scan_ports(move |net_addr: slint::SharedString| {
+        run_network_scanner(&app_ref.unwrap(), &net_addr);
     });
 
-    ui.run().unwrap();
+    app.run().unwrap();
+}
+
+fn run_network_scanner(app: &AppWindow, net_addr: &str) {
+    let output = Command::new("sudo")
+        .arg("./target/debug/scanner") // Separate binary for raw socket logic
+        .arg("-n")
+        .arg(net_addr)
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("failed to spawn scanner")
+        .wait_with_output()
+        .expect("failed to scan");
+
+    let results = String::from_utf8_lossy(&output.stdout).to_shared_string();
+    app.set_scan_results(results);
 }
